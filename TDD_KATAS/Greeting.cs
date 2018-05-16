f3e0a8a9e37d93a4b679d826c0254a6e73b81824
Ricardo Dias <cadu.hd@gmail.com>
2018-05-16 18:30:17 -0300

    add greeting tests

---
+++ TDD_KATAS/Greeting.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_KATAS
+{
+    public class Greeting
+    {
+        public static string Greet(string name)
+        {
+            name = (!string.IsNullOrEmpty(name) ? name : "my friend");
+
+            string message = $"Hello, {name}.";
+
+            return CheckIfIsUpper(name) ? message.ToUpper() : message;
+        }
+
+        private static bool CheckIfIsUpper(string name)
+        {
+            return name.All(c => char.IsUpper(c));
+        }
+
+        public static string GreetNames(string[] names)
+        {
+            string[] newNames = names.Select(n => !IsLast(n, names) ? ", " + n : ", and " + n).ToArray();
+
+            string message = $"Hello{string.Join("", newNames)}.";
+
+            return message;
+        }
+
+        private static bool IsLast(string n, string[] names)
+        {
+            return names.Last() == n;
+        }
+    }
+}
---
+++ Tests_TDD_KATAS/TestGreeting.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using TDD_KATAS;
+
+namespace Tests_TDD_KATAS
+{
+
+    class TestGreeting
+    {
+        [Test]
+        public void Greeting_ShouldGreetWithGivenName()
+        {
+            string sut = Greeting.Greet("Bob");
+
+            Assert.AreEqual(sut, "Hello, Bob.");
+        }
+
+        [Test]
+        public void Greeting_ShouldGreetWithDefaultValueIfNameIsNull()
+        {
+            string sut = Greeting.Greet(null);
+
+            Assert.AreEqual(sut, "Hello, my friend.");
+        }
+
+        [Test]
+        public void Greeting_ShouldShoutIfNameIsUppercase()
+        {
+            string sut = Greeting.Greet("JERRY");
+
+            Assert.AreEqual(sut, "HELLO, JERRY.");
+        }
+
+        [Test]
+        public void Greeting_ShouldGreetTwoPersons()
+        {
+            string[] names = new string[] { "Jill", "Jane", "" };
+
+            string sut = Greeting.GreetNames(names);
+
+            Assert.AreEqual(sut, "Hello, Jill and Jane.");
+        }
+
+        [Test]
+        public void Greeting_ShouldGreetMultiplePersons()
+        {
+            string[] names = new string[] { "Amy", "Brian", "Charlotte" };
+
+            string sut = Greeting.GreetNames(names);
+
+            Assert.AreEqual(sut, "Hello, Amy, Brian, and Charlotte.");
+        }
+    }
+}
